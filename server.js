65aac48cfe0451539ced9ad5c86a499e67abd2f0
Ana Luiza Sampaio <sampaioaanaluiza@gmail.com>
2021-09-04 13:58:22 -0300

    add config de port e db para heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./src/app")
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, ()=>{
     console.log(`alô, Pepe Moreno? to ligando na ${PORT}`)
--- src/data/dbConfig.js
+++ src/data/dbConfig.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-const MONGO_URL = process.env.MONGODB_URI 
+const MONGO_URL = process.env.MONGODB_URI  || "mongodb://localhost:27017/reprograma"
 
 const connect = () => {
     mongoose.connect(MONGO_URL, {
